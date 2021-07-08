d9687556bccc2c4014c4bd913efd7fb82ea7a7b7
Václav Zdvihal <zdvihal@gmail.com>
2021-07-08 21:29:15 +0200

    Loop All Enemy Waves

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,11 +6,15 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private List<WaveConfig> waveConfigs;
     [SerializeField] private int startingWave = 0;
+    [SerializeField] private bool looping = false;
     
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        StartCoroutine(SpawnAllWaves());
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        } while (looping);
     }
 
     private IEnumerator SpawnAllWaves()
