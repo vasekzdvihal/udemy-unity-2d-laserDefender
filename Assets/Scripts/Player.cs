2724ce7c30bdb8aabe750adf0b7fb4c718e4baf3
Vášek Zdvihal <zdvihal@gmail.com>
2021-10-23 15:01:44 +0200

    Layer Collision Matrix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -77,17 +77,18 @@ public class Player : MonoBehaviour
     
     private void OnTriggerEnter2D(Collider2D other)
     {
-        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        var damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) return;
         ProcessHit(damageDealer);
     }
 
     private void ProcessHit(DamageDealer damageDealer)
     {
         health -= damageDealer.GetDamage();
+        damageDealer.Hit();
         if (health <= 0)
         {
             Destroy(gameObject);
         }
-        damageDealer.Hit();
     }
 }
