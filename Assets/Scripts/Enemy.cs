2e473b77f46460a94a1a123c1549cb4c23106d75
Vášek Zdvihal <zdvihal@gmail.com>
2021-10-23 13:39:32 +0200

    .gitignore is now working

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float health = 100;
+    [SerializeField] private float shootCounter;
+    [SerializeField] private float minTimeBetweenShots = 0.2f;
+    [SerializeField] private float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.shootCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shootCounter -= Time.deltaTime;
+        if (shootCounter <= 0f)
+        {
+            Fire();
+            this.shootCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        var laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        // ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        damageDealer.Hit();
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // configuration parameters
+    [Header("Player")]
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] float health = 200f;
+    
+    [Header("Projectile")]
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    private Coroutine firingCoroutine;
+    
+    private float xMin;
+    private float xMax;
+    private float yMin;
+    private float yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+
+        var position = transform.position;
+        var newXPos = Mathf.Clamp(position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(position.y + deltaY, yMin, yMax);
+        
+        position = new Vector2(newXPos, newYPos);
+        transform.position = position;
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1")) firingCoroutine = StartCoroutine(FireContinuously());
+        if (Input.GetButtonUp("Fire1")) StopCoroutine(firingCoroutine);
+    }
+
+    private IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            var laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
+    }
+    
+    private void SetUpMoveBoundaries()
+    {
+        var gameCamera = Camera.main;
+        if (gameCamera is null) return;
+        
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        damageDealer.Hit();
+    }
+}
